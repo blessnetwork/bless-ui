e58e74643a1c26c190bebc82d1e39d81462e2c69
Flavio Espinoza <flavio.espinoza@gmail.com>
2025-03-05 05:32:16 -0700

    New side-nav component with new colors.

---
+++ src/app/side-nav/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Link from 'next/link'
+import SideNav from '@/components/ui/side-nav'
+import * as React from 'react'
+
+export default function GetStartedPage() {
+	return (
+		<div className="bg-[#FAFAFA]">
+      <SideNav />
+		</div>
+	)
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ const config: Config = {
 	theme: {
 		extend: {
 			colors: {
+				temp_white: '#FAF7EE',
 				background: 'hsl(var(--background))',
 				foreground: 'hsl(var(--foreground))',
 				card: {
