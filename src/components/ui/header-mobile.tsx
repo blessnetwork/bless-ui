65c041f62f652577a0a014cfbf2261214d55f39d
flavioespinoza <flavio.espinoza@gmail.com>
2025-03-16 16:36:27 -0600

    formatting

--- src/components/ui/header-mobile.tsx
+++ src/components/ui/header-mobile.tsx
@@ -4,11 +4,11 @@ export default function HeaderMobile() {
 	return (
 		<header className="flex">
 			<div className="flex-1">
-				<span className="text-white font-bold">Bless</span>
-				<span className="text-white font-bold">Online</span>
+				<span className="font-bold text-white">Bless</span>
+				<span className="font-bold text-white">Online</span>
 			</div>
 			<div className="flex-shrink-0">
-				<button className="text-white font-bold">Menu</button>
+				<button className="font-bold text-white">Menu</button>
 			</div>
 		</header>
 	)
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Header: React.FC = () => {
 	return (
 		<header className="flex w-full items-center justify-between bg-[#ff8200] p-4">
-			<span className="text-white font-bold">Bless</span>
-			<span className="text-white font-bold">Online</span>
+			<span className="font-bold text-white">Bless</span>
+			<span className="font-bold text-white">Online</span>
 		</header>
 	)
 }
